7e7db6851b3a5f34bb6fe11b949e750a456398cc
kalsteve <kalgansteve@gmail.com>
2024-01-08 16:03:30 +0900

    feat: mysql 기초 세팅

--- Backend/mysql/database.js
+++ Backend/mysql/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host : 'database-1.cds80uqge5bc.ap-northeast-2.rds.amazonaws.com',
-    user : 'admin',
-    password : 'Dnflxlaghkdlxld2023*',
-    database : 'tarotdb'
-}
+  host: "database-1.cds80uqge5bc.ap-northeast-2.rds.amazonaws.com",
+  user: "admin",
+  password: "Dnflxlaghkdlxld2023*",
+  database: "tarotdb",
+};
