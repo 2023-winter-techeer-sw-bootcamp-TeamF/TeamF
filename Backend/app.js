d9a1e49852937c3b8fa5ac3b2a3bd1efad9f13bc
kalsteve <kalgansteve@gmail.com>
2024-01-08 15:55:07 +0900

    feat: mysql 기초 설정

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // express.json(): 클라이언트로부터 오는 JSON 형식의 요청 본문을 파싱하여 JavaScript 객체로 변환.
@@ -8,46 +8,46 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // mysql 설정
-const mysql = require('mysql');
-const dbconfig = require('./mysql/database');
+const mysql = require("mysql");
+const dbconfig = require("./mysql/database");
 const connection = mysql.createConnection(dbconfig);
 // mysql 연결
 connection.connect();
 
- //swagger
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger/swagger_output.json');
+//swagger
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger/swagger_output.json");
 //swagger UI 사용 설정
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // 루트 경로 핸들러
-app.get('/', (req, res, next) => {
-  res.locals.data = { message: 'Page : app.js -> dev 테스트' };
+app.get("/", (req, res, next) => {
+  res.locals.data = { message: "Page : app.js -> dev 테스트" };
   next();
 });
 
 // API 라우트 설정
-const testRouter = require('./routes/test/test');
-app.use('/test', testRouter);
+const testRouter = require("./routes/test/test");
+app.use("/test", testRouter);
 
 // 공통 응답 미들웨어
-const commonResponseMiddleware = require('./middleware/commonResponse');
+const commonResponseMiddleware = require("./middleware/commonResponse");
 app.use(commonResponseMiddleware);
 
 // 404 핸들러(Page Not found)
 app.use((req, res, next) => {
-  res.status(404).json({ message: 'Page Not found.' });
+  res.status(404).json({ message: "Page Not found." });
 });
 
 // 오류 처리 미들웨어 (Server Error)
 app.use((error, req, res, next) => {
   console.error(error);
   res.status(500).json({
-      message: '서버 내부 오류'
+    message: "서버 내부 오류",
   });
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`서버가 포트 ${port}에서 실행`);
+  console.log(`서버가 포트 ${port}에서 실행`);
 });
