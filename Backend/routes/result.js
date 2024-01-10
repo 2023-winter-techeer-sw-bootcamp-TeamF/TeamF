8f13635703152b5e4f2def82f0c7c788b61a1b75
Heon <106370941+heondong9265@users.noreply.github.com>
2024-01-11 02:57:59 +0900

    Delete Backend/routes/result.js

--- Backend/routes/result.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const commonResponse = require('../middleware/commonResponse');
-const db = require('../mysql/database.js');
-const router = express.Router();
-
-module.exports = router;
