c06e5f0ef8fbc0fd26d0975b1672f3b065e572c3
heondong9265 <heondong0723@gmail.com>
2024-01-11 02:26:25 +0900

    feat: poll 결과 상세 조회

--- Backend/routes/result.js
+++ Backend/routes/result.js
@@ -11,4 +11,76 @@ router.get('/', (req, res, next) => {
     next();
 });
 
+router.post('/save', (req, res, next) => {
+    // Swagger 문서화
+    // #swagger.summary = "총합 결과 저장"
+    // #swagger.description = '총합 결과 저장'
+    // #swagger.tags = ['Result']
+     /*  #swagger.responses[200] = {
+              description: '테스트 값 조회 성공',}
+      } */
+    /*  #swagger.responses[400] = {
+              description: '잘못된 요청',
+      } */
+    /* #swagger.parameters['user_id'] = {
+           in: 'query',
+           description: '유저 고유번호',
+           required: true,
+           type: 'string',
+           example: '12345'
+   } */
+    /* #swagger.parameters['poll_id'] = {
+            in: 'query',
+            description: '폴 아이디',
+            required: true,
+            type: 'string',
+            example: '56789'
+    } */
+    /*
+    타로 결과 request 필요 항목
+    1. 고민 질문 v
+    2. 종합 해석 v
+    3. 폴 아이디 v
+    4. 유저 고유번호 v
+    뽑은 카드 request 필요항목
+    1. 폴 아이디 v
+    2. 카드 이미지 주소 v
+    3. 카드별 해석 v
+    4. 영어 카드 이름 v
+    5. 한글 카드 이름 v
+    */
+    const { user_id, poll_id } = req.query;
+    const { question, result_explanation, master_name, luck_type, card_image_url, card_explanation, card_eng_name, card_kor_name } = req.body;
+    // 누락 여부 체크
+    // 타로 결과 Table에 타로 결과 저장
+    const connection = db.getConnection();
+    const results_query = "INSERT INTO results (poll_id, user_id, question, explanation, master_name, luck, created_at) "
+                + "VALUES(?, ?, ?, ?, ?, ?, NOW())";
+    connection.query(results_query, [poll_id, user_id, question, result_explanation, master_name, luck_type], (error, results, fields) => {
+        if (error) {
+            res.locals.status = 500;
+            res.locals.data = { message: ' 타로 결과 Table에 데이터 저장 중 오류 발생', error };
+            return next();
+        }
+        else {
+            res.locals.data = { message : '타로 결과 Table에 데이터 저장 성공', resultsId: results.insertId };
+        }
+    });
+    // 뽑은 카드 Table에 뽑은 카드 정보 저장 (일단 카드 1장일 경우)
+    const cards_query = "INSERT INTO cards (poll_id, image_url, explanation, eng_name, kor_name, ordered, created_at) "
+                + "VALUES(?, ?, ?, ?, ?, 0, NOW())";
+    connection.query(cards_query, [poll_id, card_image_url, card_explanation, card_eng_name, card_kor_name], (error, results, fields) => {
+        if (error) {
+            res.locals.status = 500;
+            res.locals.data = { message: ' 뽑은 카드 Table에 데이터 저장 중 오류 발생', error };
+            return next();
+        }
+        else {
+            res.locals.data = { message : '뽑은 카드 Table에 데이터 저장 성공', cardsId: results.insertId };
+        }
+    });
+    res.locals.data = { message: '타로 결과, 뽑은 카드 정보 저장 성공' };
+    next();
+});
+
 module.exports = router;
--- Backend/routes/tarot.js
+++ Backend/routes/tarot.js
@@ -17,9 +17,9 @@ router.get('/guide', (req, res, next) => {
     // #swagger.summary = "가이드라인 페이지 이동 테스트"
     // #swagger.description = '가이드라인 페이지 이동 테스트'
     // #swagger.tags = ['Tarot']
-     /*  #swagger.responses[200] = {
-              description: '테스트 값 조회 성공',}
-      } */
+    /*  #swagger.responses[200] = {
+             description: '테스트 값 조회 성공',}
+     } */
     /*  #swagger.responses[400] = {
               description: '잘못된 요청',
       } */
@@ -47,7 +47,7 @@ router.get('/guide', (req, res, next) => {
         if (error) {
             res.locals.status = 500;
             res.locals.data = { message: '운 카테고리 Table에서 데이터 조회 중 오류 발생', error };
-            return next(); 
+            return next();
         }
 
         res.locals.data = { gide_line_results };
@@ -65,7 +65,7 @@ router.get('/poll/create', (req, res, next) => {
            required: true,
            type: 'string',
            example: '12345'
-   } */
+    } */
     /* #swagger.parameters['partnerid'] = {
             in: 'query',
             description: '파트너의 ID (선택 사항)',
@@ -76,12 +76,15 @@ router.get('/poll/create', (req, res, next) => {
     const { userid, partnerid } = req.query;
 
     if (!userid) {
+        /* #swagger.responses[400] = {
+            description: '요청된 사용자 ID가 누락되었을 때의 응답',
+            schema: { message: '유저 아이디 누락' }
+        } */
         res.locals.status = 400;
         res.locals.data = { message: "유저 아이디 누락" };
         res.locals.success = false;
         return next();
     }
-
 
     // 데이터베이스 연결 및 쿼리 실행
     const connection = db.getConnection();
@@ -89,14 +92,29 @@ router.get('/poll/create', (req, res, next) => {
 
     connection.query(query, [userid, partnerid], (error, results, fields) => {
         if (error) {
-            console.error('DB 저장 오류:', error);
+            /* #swagger.responses[500] = {
+                description: 'DB 저장 과정에서 오류 발생 시의 응답',
+                schema: { message: 'DB 저장 오류' }
+            } */
             res.status(500).send({ message: 'DB 저장 오류' });
         } else {
             res.locals.data = { message: 'Poll ID 생성 완료', pollId: results.insertId };
             next();
         }
     });
+    /* #swagger.responses[200] = {
+        description: 'Poll ID가 성공적으로 생성되었을 때의 응답',
+        schema: {
+            status: "success",
+            statusCode: 200,
+            data: {
+                message: 'Poll ID 생성 완료',
+                pollId: 0 
+            }
+        }
+    } */
 });
+
 
 router.post('/card/info', async (req, res, next) => {
     // #swagger.tags = ['Tarot']
@@ -111,38 +129,37 @@ router.post('/card/info', async (req, res, next) => {
     /*  #swagger.responses[500] = {
             description: '카드 정보 불러오기 실패',
     } */
-        /* #swagger.parameters['card'] = { 
+    /* #swagger.parameters['card'] = { 
         in: 'query',
         description: '카드 번호', 
         required: true,
         type: 'integer',
         example: '1'
-    }
-    */
+    } */
 
     let result = null;
     let dataObject = null;
-   const cardNum = req.query.card; // 카드 번호 저장
-    if(!cardNum) {
+    const cardNum = req.query.card; // 카드 번호 저장
+    if (!cardNum) {
         res.locals.status = 400;
         res.locals.data = { message: '카드 넘버가 없습니다.' };
         return next(); // 오류 발생 → commonResponse 미들웨어로 이동
     }
 
-    if(cardNum < 1 || cardNum > 78) {
+    if (cardNum < 1 || cardNum > 78) {
         res.locals.status = 400;
         res.locals.data = { message: '카드 넘버가 잘못되었습니다.' };
         return next(); // 오류 발생 → commonResponse 미들웨어로 이동
     }
-    try{
+    try {
         const bucketList = await s3.getbucketList(); // 연결된 S3에서 파일을 리스트로 가져옴
         const index = await s3.findIndex(bucketList, cardNum); // 카드 번호를 통해 S3에서 파일의 인덱스를 가져옴
         const fileName = await s3.getObjectName(index); // 파일명을 가져옴
         const onlyFileName = await s3.getObjectNames(bucketList, index); // 파일명을 가져옴
         dataObject = s3.getDataObject(onlyFileName); // 파일명을 통해 데이터를 가져옴
         result = await s3.getS3ImageURL(fileName); // 파일명을 통해 S3에서 이미지 주소를 가져옴
-        
-    } catch(error) {
+
+    } catch (error) {
         console.log(error);
         res.locals.status = 500;
         res.locals.data = { message: '카드 정보를 가져오는데 실패했습니다.' };
