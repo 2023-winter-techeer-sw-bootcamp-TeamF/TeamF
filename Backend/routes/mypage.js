3f0177ba2f5bdd180ae1b20ea4fbe3927ff9c03b
heondong9265 <heondong0723@gmail.com>
2024-01-11 13:59:41 +0900

    refactor: swagger 개선:

--- Backend/routes/mypage.js
+++ Backend/routes/mypage.js
@@ -7,7 +7,12 @@ router.get('/detail', (req, res) => {
     // #swagger.tags = ['MyPage']
     // #swagger.summary = "결과 리스트에서 선택한 결과 상세 조회"
     // #swagger.description = '결과 리스트 중 선택한 결과(Poll_id)를 통해 해당 결과를 상세조회한다.'
-    
+        /* #swagger.parameters['poll_id'] = { 
+        in: 'query',
+        description: '사용자의 poll_id', 
+        required: true,
+        example: '12',
+    } */   
     
     const { poll_id } = req.query;
     const connection = db.getConnection();
@@ -36,10 +41,10 @@ router.get('/detail', (req, res) => {
                 return res.status(500).send({ message: 'DB 쿼리 오류' });
             }
             /* #swagger.responses[200] = {
-                description: '결과와 카드 데이터 성공적으로 조회됨.',
+                description: '총합 결과와 카드별 해석 및 링크 데이터 성공적으로 조회됨.',
                 schema: {
-                    result: [{ question: '', explanation: '', luck: '', master_name: '', created_at: '' }],
-                    cards: [{ image_url: '', explanation: '' }]
+                    result: [{ question: '테스트 질문', explanation: '테스트 종합 해석', luck: '테스트 럭 리스트', master_name: '테스트 마스터', created_at: '2024-01-11' }],
+                    cards: [{ image_url: '테스트url', explanation: '카드해석' }]
                 }
             } */
             res.json({
