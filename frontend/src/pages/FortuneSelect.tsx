e20769347c15485ec3ab0cb10a5708690c1cdde6
이수민 <sammy1313@naver.com>
2024-01-13 14:37:48 +0900

    feat: 운선택페이지 카드 기능구현

--- frontend/src/pages/FortuneSelect.tsx
+++ frontend/src/pages/FortuneSelect.tsx
@@ -181,11 +181,7 @@ const CardText = styled.div`
   flex-direction: column;
   color: #fffbf2;
   font-family: "맑은 고딕";
-<<<<<<< HEAD
   font-size: 0.86rem;
-=======
-  font-size: 0.9375rem;
->>>>>>> 2992e41adad55caa4d4d56842103d92ce218d9fe
   width: 9rem;
   line-height: 1.3;
   margin-top: 0.6rem;
