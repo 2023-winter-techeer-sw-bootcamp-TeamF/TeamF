a411d51a72c0f5878020d25d656ad046699c48c5
heondong9265 <heondong0723@gmail.com>
2024-01-17 20:49:25 +0900

    test: init sh 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -14,10 +14,9 @@ email="tjfgkssk9472@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
+    echo "Existing data found for $domains. Replacing existing certificate."
+else
+    echo "No existing data found for $domains. Proceeding with certificate generation."
 fi
 
 
