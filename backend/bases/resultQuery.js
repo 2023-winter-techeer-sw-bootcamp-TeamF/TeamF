db4afe997bef0632cadcd4e8fd361cebf5e78220
김민기 <mocodoco@naver.com>
2024-01-22 23:38:11 +0900

    SRP 리팩토링 2차 수정

--- backend/bases/resultQuery.js
+++ backend/bases/resultQuery.js
@@ -6,14 +6,13 @@ const router = express.Router();
 
 const resultQuery = async (connection, res, poll_id, next) => {
   try {
+    const connection = db.getConnection();
     const query =
       "SELECT question, explanation, luck, master_name FROM result WHERE poll_id = ?";
     const data = await new Promise((resolve, rejects) => {
       connection.query(query, [poll_id], (error, resultData) => {
         if (error) {
           console.error(error);
-          res.locals.status = 500;
-          res.locals.data = { message: "DB 쿼리 오류", error: error.message };
           rejects(new Error("DB 오류: result에서 데이터 조회 중 오류 발생"));
         }
         resolve(resultData);
@@ -23,9 +22,7 @@ const resultQuery = async (connection, res, poll_id, next) => {
     return data.length > 0 ? data : "데이터가 없음";
   } catch (error) {
     console.error(error);
-    res.locals.data = { message: error.message };
-    res.locals.status = 500;
-    return next();
+    throw error;
   }
 };
 
