930645f6c9c055cbcddca49d845dd3bca0ad48c6
Heon <106370941+heondong9265@users.noreply.github.com>
2024-01-22 13:42:23 +0900

    Update swagger.js

--- backend/swagger/swagger.js
+++ backend/swagger/swagger.js
@@ -11,7 +11,7 @@ const doc = {
    * (주의) main 배포는...3000으로 해야하나... 여기까지 알아보기는 마음이 지금 아픔
    */
   //host: "localhost:3000",
-  host: "43.202.208.226:3001",
+  host: "43.202.208.226:3000",
   //host: "tairot.online",
   schemes: ["http"],
   securityDefinitions: {
